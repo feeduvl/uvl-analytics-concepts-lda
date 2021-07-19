075da913922308c1d81c3d269b946f2ee8151c8e
Johannes Daub <polynoman@live.de>
2021-07-19 16:53:02 +0200

    Improve preprocessing

--- data_process.py
+++ data_process.py
@@ -11,8 +11,8 @@ def preprocess(dataset, stemming=False):
     data = []
     for entry in dataset:
         t = pp.preprocess_string(entry['text'].lower(), filters=[pp.strip_tags, pp.strip_punctuation,
-                                                                 pp.strip_multiple_whitespaces, pp.strip_numeric,
-                                                                 pp.remove_stopwords])
+                                                                 pp.strip_non_alphanum, pp.strip_multiple_whitespaces,
+                                                                 pp.strip_numeric, pp.remove_stopwords])
         if stemming:
             t = pp.preprocess_string(t, filters=[pp.stem_text])
 
